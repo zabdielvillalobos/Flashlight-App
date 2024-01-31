eb35fe9f1b97b51f1bc0e83d1f51c095ff463ffa
Zabdiel Villalobos <zabdielv@ad.unc.edu>
2024-01-31 18:34:33 +0000

    Setting up GitHub Classroom Feedback

---
+++ Lighty/Lighty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lighty
+//
+//  Created by Zabdiel Villalobos on 1/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lighty/Lighty/LightyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightyApp.swift
+//  Lighty
+//
+//  Created by Zabdiel Villalobos on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct LightyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
